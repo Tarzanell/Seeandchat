21b5f3c742ed29f29787ad1d1b92cc53ffba9abb
Tarzanell <nemiades.val@gmail.com>
2025-03-16 21:12:42 +0100

    Aggiunte pagine personaggi e login, non va selezione personaggi

---
+++ seeandchat-client/src/pages/Characters.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Characters() {
+  const [characters, setCharacters] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      const token = localStorage.getItem("token");
+      const response = await fetch("http://217.154.16.188:3001/api/personaggi", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      console.log("Dati ricevuti dal server:", data); // 👈 DEBUG: stampa i dati ricevuti
+      if (response.ok) {
+        setCharacters(data);
+      } else {
+        alert("Errore nel recupero dei personaggi.");
+      }
+    };
+
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div>
+      <h2>Seleziona un personaggio</h2>
+      <ul>
+        {characters.map((char) => (
+          <li key={char.id} onClick={() => navigate(`/character/${char.id}`)}>
+            {char.nome}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Characters;
